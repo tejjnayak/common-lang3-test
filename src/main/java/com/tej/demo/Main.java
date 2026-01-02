daab44a96b85b2666a6cf728a6061c1df4707f6f
Tejaswini Nayak <tnayak@endor.ai>
2026-01-02 10:16:40 +0530

    update

---
+++ src/main/java/com/tej/demo/Main.java
@@ -0,0 +1,12 @@
+package com.tej.demo;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "   Commons Lang3 in Git   ";
+        System.out.println(StringUtils.trim(text));
+        System.out.println(StringUtils.reverse("Tej"));
+    }
+}
+
